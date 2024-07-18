43c7897167ff777373f926593228d520c88f118c
Ghost0159 <contact.ghost01@gmail.com>
2024-07-18 04:25:45 +0200

    add retro for merge

--- merge.py
+++ merge.py
@@ -2,20 +2,21 @@ import json
 import os
 
 # Directories to traverse
-directories = ['base', 'dlc', 'update']
+directories = ['base', 'dlc', 'update', 'retro']
 
 # Dictionary to store the combined data
 combined_data = {"titledb": {}}
 
 # Traverse each directory
 for directory in directories:
-    for filename in os.listdir(directory):
-        if filename.endswith(".json"):
-            filepath = os.path.join(directory, filename)
-            with open(filepath, 'r') as f:
-                game_data = json.load(f)
-                game_id = game_data["id"]
-                combined_data["titledb"][game_id] = game_data
+    for root, _, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith(".json"):
+                filepath = os.path.join(root, filename)
+                with open(filepath, 'r') as f:
+                    game_data = json.load(f)
+                    game_id = game_data["id"]
+                    combined_data["titledb"][game_id] = game_data
 
 # Write the combined data to fulldb.json
 with open('fulldb.json', 'w') as outfile:
